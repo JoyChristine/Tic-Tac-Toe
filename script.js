994b59030dd662be8c533744277090cc9f6c7208
Joy Christine <joychristin2@gmail.com>
2020-09-22 15:43:15 +0300

    restart button is working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
-const winning_combos = [
+const WINNING_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -13,29 +13,63 @@ const winning_combos = [
 ]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('win-msg')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn
 
 BeginGame()
 
+restartButton.addEventListener('click', BeginGame)
+
 function BeginGame() {
     circleTurn = false
     cellElements.forEach(cell => {
-            cell.addEventListener('click', handleClick, { once: true })
-        })
-        // hover effect at the start of the game
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click', handleClick)
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass() // hover effect at the start of the game
+    winningMessageElement.classList.remove('show') //restart btn works bt doesn't clear the X's and O's
+
 }
 
 function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+        // console.log("winner") 
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns()
+        setBoardHoverClass()
 
-    //check win
-    //check draw
-    //switch turns 
-    swapTurns()
-    setBoardHoverClass()
+    }
+}
+//check win
+//check draw
+//switch turns 
 
+
+
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!'
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+}
+//f all cells contain o's or x's return draw
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
 }
 //placemark
 function placeMark(cell, currentClass) {
@@ -54,4 +88,12 @@ function setBoardHoverClass() {
     } else {
         board.classList.add(X_CLASS)
     }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combinations => {
+        return combinations.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
 }
